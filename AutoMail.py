b34aa019bc8a7ad39028faafcbc81eacd5ff7b70
Florian Becher <becher.florian@outlook.com>
2017-05-16 10:23:36 +0200

    added gitignore auto mailer sends mail and get username and pw from file

--- AutoMail.py
+++ AutoMail.py
@@ -1,21 +1,18 @@
 import smtplib
 
 
-file = open('C:\Daten\projects\python\AutoBot\password', 'r')
-first_line = file.readline()
-print(first_line)
-
-
-#username = 'becher.florian97@gmail.com'
+file = open(".\password", 'r')
+username = file.readline()
+pw = file.readline()
 
 # Create a text/plain message
-#msg = "Just a test"
+msg = "Just a test"
 
-#me = 'Test@testing.ch'
-#you = 'becher.florian@outlook.com'
+me = 'Test@testing.ch'
+you = 'becher.florian@outlook.com'
 
-#server = smtplib.SMTP('smtp.gmail.com:587')
-#server.starttls()
-#server.login(username, pw)
-#server.sendmail(me, you, msg)
-#server.quit()
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username, pw)
+server.sendmail(me, you, msg)
+server.quit()
