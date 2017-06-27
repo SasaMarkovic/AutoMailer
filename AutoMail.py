8e3658cbbc7a32703501ff7df120bf7ef8ac0618
Florian Becher <becher.florian@outlook.com>
2017-06-27 08:27:21 +0200

    bugfix

--- AutoMail.py
+++ AutoMail.py
@@ -11,7 +11,7 @@ def sendMail(body):
     pw = file.readline()
 
     # Create a text/plain message
-    msg = "Sorry but the User 'Florian Becher' is not available" + body
+    msg = "Sorry but the User 'Florian Becher' is not available\n\n\n" + body
 
     #Mail from who? -me (Fastresponde@me.com)
     me = 'FastResponde@me.com'
@@ -25,6 +25,8 @@ def sendMail(body):
     server.sendmail(me, you, msg)
     server.quit()
 
+    print("EMAIL SEND!")
+
 #Looks if a mail is in the inbox and checks sender of it
 #If is is from YOU it send answer
 def checkMail():
@@ -33,20 +35,26 @@ def checkMail():
     inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
                                         # the inbox. You can change that number to reference
                                         # any other folder
+
+                                        #3 Deleted
     messages = inbox.Items
     message = messages.GetLast()
     sender = message.SenderEmailAddress
     body_content = message.body # BODY INHALT USW.
+    body = "Hello and so on"
+    name = sender.rpartition('@')[0]
+    firstname = name.split('.')[0]
+    secondname = name.rpartition('.')[2]
 
-    print(sender)
+    print(sender, firstname, secondname)
 
     if sender == "becher.florian97@gmail.com":
         print("Email sender will be activated!")
-        sendMail(body_content)
+        sendMail(body)
     else:
         print("Not the Email to respond!")
 
 #Loop for Mail
 while True:
     checkMail()
-    time.sleep(300)
+    time.sleep(10)
